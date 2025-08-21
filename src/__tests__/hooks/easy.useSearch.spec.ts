5b75689c0add2e96c1aeab76cf63900e2f0a32dd
yangchanghun <gory4848@naver.com>
2025-08-21 23:25:30 +0900

    useSearch test작성

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -41,16 +41,112 @@ const mockEvents: Event[] = [
   },
 ];
 
+// export const useSearch = (events: Event[], currentDate: Date, view: 'week' | 'month') => {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const filteredEvents = useMemo(() => {
+//     return getFilteredEvents(events, searchTerm, currentDate, view);
+//   }, [events, searchTerm, currentDate, view]);
+
+//   return {
+//     searchTerm,
+//     setSearchTerm,
+//     filteredEvents,
+//   };
+// };
+
 // it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {
 //   const { result } = renderHook(() => useSearch(mockEvents, new, 'month'));
 
 //   expect(result.current.filteredEvents).toEqual(mockEvents);
 // });
 
-it('검색어에 맞는 이벤트만 필터링해야 한다', () => {});
+it('검색어에 맞는 이벤트만 필터링해야 한다', () => {
+  const { result } = renderHook(() => useSearch(mockEvents, new Date('2025-08-19'), 'month'));
+  console.log(result.current.filteredEvents);
+  expect(result.current.filteredEvents).toEqual([
+    {
+      id: '1',
+      title: '면접공부',
+      date: '2025-08-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '면접 준비',
+      location: '내 방',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+    {
+      id: '2',
+      title: '회의',
+      date: '2025-08-19',
+      startTime: '12:00',
+      endTime: '13:00',
+      description: '회의',
+      location: '카페',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+    {
+      id: '3',
+      title: '코테풀기',
+      date: '2025-08-19',
+      startTime: '18:00',
+      endTime: '19:00',
+      description: '코테 풀기',
+      location: '릿코드',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+  ]);
+});
 
-it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {});
+it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(mockEvents, new Date('2025-08-19'), 'month'));
 
-it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {});
+  act(() => {
+    result.current.setSearchTerm('회의');
+  });
 
-it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {});
+  expect(result.current.filteredEvents).toEqual([
+    {
+      id: '2',
+      title: '회의',
+      date: '2025-08-19',
+      startTime: '12:00',
+      endTime: '13:00',
+      description: '회의',
+      location: '카페',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+  ]);
+});
+
+it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(mockEvents, new Date('2025-08-19'), 'week'));
+
+  act(() => {
+    result.current.setSearchTerm('코테풀기');
+  });
+
+  expect(result.current.filteredEvents).toEqual([mockEvents[2]]);
+});
+
+it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
+  const { result } = renderHook(() => useSearch(mockEvents, new Date('2025-08-19'), 'month'));
+  act(() => {
+    result.current.setSearchTerm('회의');
+  });
+  expect(result.current.filteredEvents).toEqual([mockEvents[1]]);
+
+  act(() => {
+    result.current.setSearchTerm('코테 풀기');
+  });
+
+  expect(result.current.filteredEvents).toEqual([mockEvents[2]]);
+});
